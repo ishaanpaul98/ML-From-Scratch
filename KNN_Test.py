00325b216932bb77b3196e38747f76482b5e5405
Ishaan Paul <ishaanpaul@outlook.com>
2023-08-31 09:58:03 -0700

    Completed implementation in knn.py

--- KNN_Test.py
+++ KNN_Test.py
@@ -9,3 +9,13 @@ iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1234)
+
+print(X_train.shape)
+print(X_train[0])
+
+print(y_train.shape)
+print(y_train)
+
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], c = y, cmap = cmap, edgecolor = 'k', s = 20)
+plt.show()
