4f5efc65c4b650163b3a1666f119c00bad81a9db
Ishaan Paul <ishaanpaul@outlook.com>
2023-08-31 10:00:58 -0700

    Completed implementation of KNN and tested

--- KNN_Test.py
+++ KNN_Test.py
@@ -10,12 +10,19 @@ X, y = iris.data, iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1234)
 
-print(X_train.shape)
-print(X_train[0])
+#print(X_train.shape)
+#print(X_train[0])
 
-print(y_train.shape)
-print(y_train)
+#print(y_train.shape)
+#print(y_train)
 
-plt.figure()
-plt.scatter(X[:, 0], X[:, 1], c = y, cmap = cmap, edgecolor = 'k', s = 20)
-plt.show()
+#plt.figure()
+#plt.scatter(X[:, 0], X[:, 1], c = y, cmap = cmap, edgecolor = 'k', s = 20)
+#plt.show()
+
+from KNN import KNN
+knn = KNN(k=5)
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+accuracy = np.sum(predictions==y_test) / len(y_test)
+print(accuracy)
