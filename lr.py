a667b66ee3f1835a583d82ec4888fd9e9330483e
Ishaan Paul <ishaanpaul@outlook.com>
2023-08-31 10:35:52 -0700

    Create empty class functions for Linear Regression

---
+++ lr.py
@@ -0,0 +1,13 @@
+class LinearRegression:
+    def __init__(self, lr = 0.001, n_iters = 1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
+    
