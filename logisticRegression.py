99902e02d2790fe3b2af1136ad5a8a9a638a927d
Ishaan Paul <ishaanpaul@outlook.com>
2023-09-05 09:45:48 -0700

    Created empty class functions in Linear Regression

---
+++ logisticRegression.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class logisticRegression:
+    def __init__(self, lr = 0.001, num_iters = 1000):
+        self.lr = lr
+        self.num_iters = num_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
