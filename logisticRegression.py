a632a41b85cdb54d441fb4d8bbe1aa45d9a9b8c6
Ishaan Paul <ishaanpaul@outlook.com>
2023-09-05 11:05:38 -0700

    Completed class functions in linearRegression

--- logisticRegression.py
+++ logisticRegression.py
@@ -8,7 +8,24 @@ class logisticRegression:
         self.bias = None
 
     def fit(self, X, y):
-        pass
+        num_samples, num_features = X.shape
+        self.weights = np.zeros(num_features)
+        self.bias = 0
+
+        #Gradient descent
+        for i in range(self.num_iters):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_predicted = self._sigmoid(linear_model)
+            dw = (1 / num_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / num_samples) * np.sum(y_predicted - y)
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
 
     def predict(self, X):
-        pass
+        linear_model = np.dot(X, self.weights) + self.bias
+        y_predicted = self._sigmoid(linear_model)
+        y_predicted_class = [1 if i > 0.5 else 0 for i in y_predicted]
+        return y_predicted_class
+
+    def _sigmoid(self, x):
+        return (1/ (1 + np.exp(-x)))
